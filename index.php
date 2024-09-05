5d4fc42e0dada164f2d673e23d70ee41f6e91cbc
niraj1234 <niraj.webtech@gmail.com>
2024-09-05 14:50:36 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<html><title>PHP RnD</title>
+<body>
+
+
+<h2>PHP Applied RnD</h2>
+<?php 
+  $siteName = "www.PatnaInfo.com" ;
+  echo "Hello Niraj. Welcome to <b> PHP Applied RnD </b> !"
+  echo "<br><br>";
+  print $siteName ;
+?>
+
+
+
+</body>
+</html>
+
